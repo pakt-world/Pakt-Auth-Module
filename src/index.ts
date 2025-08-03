0b5a4ae7f036c050b9d0a8f339cb1706d6fd5971
Paul-Taiwo <ayopaulot@gmail.com>
2025-08-03 17:12:52 +0100

    feat: integrate login

--- src/index.ts
+++ src/index.ts
@@ -7,12 +7,17 @@ export * from "./utils";
 // Authentication components
 export * from "./components/auth";
 export { default as PaktAuth } from "./components/pakt-auth";
-export type { PaktAuthRef } from "./components/pakt-auth";
+export type { AuthRef } from "./components/pakt-auth/types";
 
 // Authentication utilities and hooks
 export * from "./utils/auth-utils";
 export * from "./utils/validation";
 export * from "./hooks/use-google-auth";
+export { usePaktAuth } from "./hooks/use-pakt-auth";
+
+// PAKT SDK
+export { paktSDKService } from "./lib/pakt-sdk";
+export type { PaktSDKConfig, AuthResponse } from "./lib/pakt-sdk";
 
 // Authentication types
 export * from "./types/auth";
